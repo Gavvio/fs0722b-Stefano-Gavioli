b3f940008190a9761889f0470093c21f787df298
Gavvio <steve.gavvio@gmail.com>
2022-12-21 16:31:37 +0100

    ora va meglio

--- m1/w9/d3/script.js
+++ m1/w9/d3/script.js
@@ -20,205 +20,164 @@ var Persona = /** @class */ (function () {
         this.tasseInps = 0;
         this.tasseIrpef = 0;
     }
+    Persona.prototype.getTasseInps = function (tasse) {
+        return tasse * 40 / 100;
+    };
+    Persona.prototype.getTasseIrpef = function (tasse) {
+        return tasse * 60 / 100;
+    };
+    Persona.prototype.getRedditoAnnuoNetto = function (tasse) {
+        return this.redditoAnnuoLordo - tasse;
+    };
     return Persona;
 }());
 var Bagnino = /** @class */ (function (_super) {
     __extends(Bagnino, _super);
     function Bagnino(b) {
-        var _this = _super.call(this, 1001, b) || this;
-        _this.totaleTasse = 0;
-        return _this;
+        return _super.call(this, 1001, b) || this;
     }
     Bagnino.prototype.getUtileTasse = function () {
         if (this.codredd == 1001) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 5) / 100;
+            return (this.redditoAnnuoLordo * 5) / 100;
         }
         else if (this.codredd == 1002) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 10) / 100;
+            return (this.redditoAnnuoLordo * 10) / 100;
         }
         else if (this.codredd == 1003) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 15) / 100;
+            return (this.redditoAnnuoLordo * 15) / 100;
         }
         else if (this.codredd == 1004) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 20) / 100;
+            return (this.redditoAnnuoLordo * 20) / 100;
         }
         else if (this.codredd == 1005) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 25) / 100;
+            return (this.redditoAnnuoLordo * 25) / 100;
         }
         else {
             console.log("there's been an error");
+            return 0;
         }
-    };
-    Bagnino.prototype.getTasseInps = function () {
-        return this.totaleTasse * 40 / 100;
-    };
-    Bagnino.prototype.getTasseIrpef = function () {
-        return this.totaleTasse * 60 / 100;
-    };
-    Bagnino.prototype.getRedditoAnnuoNetto = function () {
-        return this.redditoAnnuoLordo - this.totaleTasse;
     };
     return Bagnino;
 }(Persona));
 var Cassiere = /** @class */ (function (_super) {
     __extends(Cassiere, _super);
     function Cassiere(b) {
-        var _this = _super.call(this, 1002, b) || this;
-        _this.totaleTasse = 0;
-        return _this;
+        return _super.call(this, 1002, b) || this;
     }
     Cassiere.prototype.getUtileTasse = function () {
         if (this.codredd == 1001) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 5) / 100;
+            return (this.redditoAnnuoLordo * 5) / 100;
         }
         else if (this.codredd == 1002) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 10) / 100;
+            return (this.redditoAnnuoLordo * 10) / 100;
         }
         else if (this.codredd == 1003) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 15) / 100;
+            return (this.redditoAnnuoLordo * 15) / 100;
         }
         else if (this.codredd == 1004) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 20) / 100;
+            return (this.redditoAnnuoLordo * 20) / 100;
         }
         else if (this.codredd == 1005) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 25) / 100;
+            return (this.redditoAnnuoLordo * 25) / 100;
         }
         else {
             console.log("there's been an error");
+            return 0;
         }
-    };
-    Cassiere.prototype.getTasseInps = function () {
-        return this.totaleTasse * 40 / 100;
-    };
-    Cassiere.prototype.getTasseIrpef = function () {
-        return this.totaleTasse * 60 / 100;
-    };
-    Cassiere.prototype.getRedditoAnnuoNetto = function () {
-        return this.redditoAnnuoLordo - this.totaleTasse;
     };
     return Cassiere;
 }(Persona));
 var Direttore = /** @class */ (function (_super) {
     __extends(Direttore, _super);
     function Direttore(b) {
-        var _this = _super.call(this, 1003, b) || this;
-        _this.totaleTasse = 0;
-        return _this;
+        return _super.call(this, 1003, b) || this;
     }
     Direttore.prototype.getUtileTasse = function () {
         if (this.codredd == 1001) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 5) / 100;
+            return (this.redditoAnnuoLordo * 5) / 100;
         }
         else if (this.codredd == 1002) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 10) / 100;
+            return (this.redditoAnnuoLordo * 10) / 100;
         }
         else if (this.codredd == 1003) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 15) / 100;
+            return (this.redditoAnnuoLordo * 15) / 100;
         }
         else if (this.codredd == 1004) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 20) / 100;
+            return (this.redditoAnnuoLordo * 20) / 100;
         }
         else if (this.codredd == 1005) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 25) / 100;
+            return (this.redditoAnnuoLordo * 25) / 100;
         }
         else {
             console.log("there's been an error");
+            return 0;
         }
-    };
-    Direttore.prototype.getTasseInps = function () {
-        return this.totaleTasse * 40 / 100;
-    };
-    Direttore.prototype.getTasseIrpef = function () {
-        return this.totaleTasse * 60 / 100;
-    };
-    Direttore.prototype.getRedditoAnnuoNetto = function () {
-        return this.redditoAnnuoLordo - this.totaleTasse;
     };
     return Direttore;
 }(Persona));
 var Calciatore = /** @class */ (function (_super) {
     __extends(Calciatore, _super);
     function Calciatore(b) {
-        var _this = _super.call(this, 1004, b) || this;
-        _this.totaleTasse = 0;
-        return _this;
+        return _super.call(this, 1004, b) || this;
     }
     Calciatore.prototype.getUtileTasse = function () {
         if (this.codredd == 1001) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 5) / 100;
+            return (this.redditoAnnuoLordo * 5) / 100;
         }
         else if (this.codredd == 1002) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 10) / 100;
+            return (this.redditoAnnuoLordo * 10) / 100;
         }
         else if (this.codredd == 1003) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 15) / 100;
+            return (this.redditoAnnuoLordo * 15) / 100;
         }
         else if (this.codredd == 1004) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 20) / 100;
+            return (this.redditoAnnuoLordo * 20) / 100;
         }
         else if (this.codredd == 1005) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 25) / 100;
+            return (this.redditoAnnuoLordo * 25) / 100;
         }
         else {
             console.log("there's been an error");
+            return 0;
         }
-    };
-    Calciatore.prototype.getTasseInps = function () {
-        return this.totaleTasse * 40 / 100;
-    };
-    Calciatore.prototype.getTasseIrpef = function () {
-        return this.totaleTasse * 60 / 100;
-    };
-    Calciatore.prototype.getRedditoAnnuoNetto = function () {
-        return this.redditoAnnuoLordo - this.totaleTasse;
     };
     return Calciatore;
 }(Persona));
 var Riccone = /** @class */ (function (_super) {
     __extends(Riccone, _super);
     function Riccone(b) {
-        var _this = _super.call(this, 1005, b) || this;
-        _this.totaleTasse = 0;
-        return _this;
+        return _super.call(this, 1005, b) || this;
     }
     Riccone.prototype.getUtileTasse = function () {
         if (this.codredd == 1001) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 5) / 100;
+            return (this.redditoAnnuoLordo * 5) / 100;
         }
         else if (this.codredd == 1002) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 10) / 100;
+            return (this.redditoAnnuoLordo * 10) / 100;
         }
         else if (this.codredd == 1003) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 15) / 100;
+            return (this.redditoAnnuoLordo * 15) / 100;
         }
         else if (this.codredd == 1004) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 20) / 100;
+            return (this.redditoAnnuoLordo * 20) / 100;
         }
         else if (this.codredd == 1005) {
-            this.totaleTasse = (this.redditoAnnuoLordo * 25) / 100;
+            return (this.redditoAnnuoLordo * 25) / 100;
         }
         else {
             console.log("there's been an error");
+            return 0;
         }
-    };
-    Riccone.prototype.getTasseInps = function () {
-        return this.totaleTasse * 40 / 100;
-    };
-    Riccone.prototype.getTasseIrpef = function () {
-        return this.totaleTasse * 60 / 100;
-    };
-    Riccone.prototype.getRedditoAnnuoNetto = function () {
-        return this.redditoAnnuoLordo - this.totaleTasse;
     };
     return Riccone;
 }(Persona));
 var bagnino = new Bagnino(13000);
-console.log(bagnino.getUtileTasse(), bagnino.totaleTasse, bagnino.getTasseInps(), bagnino.getTasseIrpef(), bagnino.getRedditoAnnuoNetto());
+console.log(bagnino.getUtileTasse(), bagnino.getTasseInps(bagnino.getUtileTasse()), bagnino.getTasseIrpef(bagnino.getUtileTasse()), bagnino.getRedditoAnnuoNetto(bagnino.getUtileTasse()));
 var cassiere = new Cassiere(20000);
-console.log(cassiere.getUtileTasse(), cassiere.totaleTasse, cassiere.getTasseInps(), cassiere.getTasseIrpef(), cassiere.getRedditoAnnuoNetto());
+console.log(cassiere.getUtileTasse(), cassiere.getTasseInps(cassiere.getUtileTasse()), cassiere.getTasseIrpef(cassiere.getUtileTasse()), cassiere.getRedditoAnnuoNetto(cassiere.getUtileTasse()));
 var direttore = new Direttore(50000);
-console.log(direttore.getUtileTasse(), direttore.totaleTasse, direttore.getTasseInps(), direttore.getTasseIrpef(), direttore.getRedditoAnnuoNetto());
+console.log(direttore.getUtileTasse(), direttore.getTasseInps(direttore.getUtileTasse()), direttore.getTasseIrpef(direttore.getUtileTasse()), direttore.getRedditoAnnuoNetto(direttore.getUtileTasse()));
 var calciatore = new Calciatore(60000);
-console.log(calciatore.getUtileTasse(), calciatore.totaleTasse, calciatore.getTasseInps(), calciatore.getTasseIrpef(), calciatore.getRedditoAnnuoNetto());
+console.log(calciatore.getUtileTasse(), calciatore.getTasseInps(calciatore.getUtileTasse()), calciatore.getTasseIrpef(calciatore.getUtileTasse()), calciatore.getRedditoAnnuoNetto(calciatore.getUtileTasse()));
 var riccone = new Riccone(100000);
-console.log(riccone.getUtileTasse(), riccone.totaleTasse, riccone.getTasseInps(), riccone.getTasseIrpef(), riccone.getRedditoAnnuoNetto());
+console.log(riccone.getUtileTasse(), riccone.getTasseInps(riccone.getUtileTasse()), riccone.getTasseIrpef(riccone.getUtileTasse()), riccone.getRedditoAnnuoNetto(riccone.getUtileTasse()));
--- m1/w9/d3/script.ts
+++ m1/w9/d3/script.ts
@@ -1,184 +1,146 @@
-abstract class Persona{
-    codredd:number;
-    redditoAnnuoLordo:number;
-    tasseInps:number;
-    tasseIrpef:number;
-    constructor(codice:number,soldi:number){
-        this.codredd=codice;
-        this.redditoAnnuoLordo=soldi;
-        this.tasseInps=0;
-        this.tasseIrpef=0;
+abstract class Persona {
+    codredd: number;
+    redditoAnnuoLordo: number;
+    tasseInps: number;
+    tasseIrpef: number;
+    constructor(codice: number, soldi: number) {
+        this.codredd = codice;
+        this.redditoAnnuoLordo = soldi;
+        this.tasseInps = 0;
+        this.tasseIrpef = 0;
     }
+    getTasseInps(tasse: number) {
+        return tasse * 40 / 100;
+    }
+    getTasseIrpef(tasse: number) {
+        return tasse * 60 / 100;
+    }
+    getRedditoAnnuoNetto(tasse: number) {
+        return this.redditoAnnuoLordo - tasse;
+    }
+    abstract getUtileTasse(): number;
 }
 
 
-class Bagnino extends Persona{
-    totaleTasse:number;
-    constructor(b:number){
-        super(1001,b)
-       this.totaleTasse=0;
+class Bagnino extends Persona {
+    constructor(b: number) {
+        super(1001, b)
     }
-    getUtileTasse(){
-        if(this.codredd == 1001){
-            this.totaleTasse=(this.redditoAnnuoLordo * 5) / 100
-        }else if(this.codredd == 1002){
-            this.totaleTasse=(this.redditoAnnuoLordo * 10) / 100
-        }else if(this.codredd == 1003){
-            this.totaleTasse=(this.redditoAnnuoLordo * 15) / 100
-        }else if(this.codredd == 1004){
-            this.totaleTasse=(this.redditoAnnuoLordo * 20) / 100
-        }else if(this.codredd == 1005){
-            this.totaleTasse=(this.redditoAnnuoLordo * 25) / 100
-        }else {
+    getUtileTasse(): number {
+        if (this.codredd == 1001) {
+            return (this.redditoAnnuoLordo * 5) / 100
+        } else if (this.codredd == 1002) {
+            return (this.redditoAnnuoLordo * 10) / 100
+        } else if (this.codredd == 1003) {
+            return (this.redditoAnnuoLordo * 15) / 100
+        } else if (this.codredd == 1004) {
+            return (this.redditoAnnuoLordo * 20) / 100
+        } else if (this.codredd == 1005) {
+            return (this.redditoAnnuoLordo * 25) / 100
+        } else {
             console.log("there's been an error")
+            return 0;
         }
     }
-    getTasseInps(){
-        return this.totaleTasse*40/100;
+
+}
+
+class Cassiere extends Persona {
+    constructor(b: number) {
+        super(1002, b)
     }
-    getTasseIrpef(){
-        return this.totaleTasse*60/100;
-    }
-    getRedditoAnnuoNetto(){
-        return this.redditoAnnuoLordo-this.totaleTasse;
+    getUtileTasse(): number {
+        if (this.codredd == 1001) {
+            return (this.redditoAnnuoLordo * 5) / 100
+        } else if (this.codredd == 1002) {
+            return (this.redditoAnnuoLordo * 10) / 100
+        } else if (this.codredd == 1003) {
+            return (this.redditoAnnuoLordo * 15) / 100
+        } else if (this.codredd == 1004) {
+            return (this.redditoAnnuoLordo * 20) / 100
+        } else if (this.codredd == 1005) {
+            return (this.redditoAnnuoLordo * 25) / 100
+        } else {
+            console.log("there's been an error")
+            return 0;
+        }
     }
 }
 
-class Cassiere extends Persona{
-    totaleTasse:number;
-    constructor(b:number){
-        super(1002,b)
-       this.totaleTasse=0;
+class Direttore extends Persona {
+    constructor(b: number) {
+        super(1003, b)
     }
-    getUtileTasse(){
-        if(this.codredd == 1001){
-            this.totaleTasse=(this.redditoAnnuoLordo * 5) / 100
-        }else if(this.codredd == 1002){
-            this.totaleTasse=(this.redditoAnnuoLordo * 10) / 100
-        }else if(this.codredd == 1003){
-            this.totaleTasse=(this.redditoAnnuoLordo * 15) / 100
-        }else if(this.codredd == 1004){
-            this.totaleTasse=(this.redditoAnnuoLordo * 20) / 100
-        }else if(this.codredd == 1005){
-            this.totaleTasse=(this.redditoAnnuoLordo * 25) / 100
-        }else {
+    getUtileTasse(): number {
+        if (this.codredd == 1001) {
+            return (this.redditoAnnuoLordo * 5) / 100
+        } else if (this.codredd == 1002) {
+            return (this.redditoAnnuoLordo * 10) / 100
+        } else if (this.codredd == 1003) {
+            return (this.redditoAnnuoLordo * 15) / 100
+        } else if (this.codredd == 1004) {
+            return (this.redditoAnnuoLordo * 20) / 100
+        } else if (this.codredd == 1005) {
+            return (this.redditoAnnuoLordo * 25) / 100
+        } else {
             console.log("there's been an error")
+            return 0;
         }
-    }
-    getTasseInps(){
-        return this.totaleTasse*40/100;
-    }
-    getTasseIrpef(){
-        return this.totaleTasse*60/100;
-    }
-    getRedditoAnnuoNetto(){
-        return this.redditoAnnuoLordo-this.totaleTasse;
     }
 }
 
-class Direttore extends Persona{
-    totaleTasse:number;
-    constructor(b:number){
-        super(1003,b)
-       this.totaleTasse=0;
+class Calciatore extends Persona {
+
+    constructor(b: number) {
+        super(1004, b)
     }
-    getUtileTasse(){
-        if(this.codredd == 1001){
-            this.totaleTasse=(this.redditoAnnuoLordo * 5) / 100
-        }else if(this.codredd == 1002){
-            this.totaleTasse=(this.redditoAnnuoLordo * 10) / 100
-        }else if(this.codredd == 1003){
-            this.totaleTasse=(this.redditoAnnuoLordo * 15) / 100
-        }else if(this.codredd == 1004){
-            this.totaleTasse=(this.redditoAnnuoLordo * 20) / 100
-        }else if(this.codredd == 1005){
-            this.totaleTasse=(this.redditoAnnuoLordo * 25) / 100
-        }else {
+    getUtileTasse():number{
+        if (this.codredd == 1001) {
+            return (this.redditoAnnuoLordo * 5) / 100
+        } else if (this.codredd == 1002) {
+            return (this.redditoAnnuoLordo * 10) / 100
+        } else if (this.codredd == 1003) {
+            return (this.redditoAnnuoLordo * 15) / 100
+        } else if (this.codredd == 1004) {
+            return (this.redditoAnnuoLordo * 20) / 100
+        } else if (this.codredd == 1005) {
+            return (this.redditoAnnuoLordo * 25) / 100
+        } else {
             console.log("there's been an error")
+            return 0;
         }
-    }
-    getTasseInps(){
-        return this.totaleTasse*40/100;
-    }
-    getTasseIrpef(){
-        return this.totaleTasse*60/100;
-    }
-    getRedditoAnnuoNetto(){
-        return this.redditoAnnuoLordo-this.totaleTasse;
     }
 }
 
-class Calciatore extends Persona{
-    totaleTasse:number;
-    constructor(b:number){
-        super(1004,b)
-       this.totaleTasse=0;
+class Riccone extends Persona {
+    constructor(b: number) {
+        super(1005, b)
     }
-    getUtileTasse(){
-        if(this.codredd == 1001){
-            this.totaleTasse=(this.redditoAnnuoLordo * 5) / 100
-        }else if(this.codredd == 1002){
-            this.totaleTasse=(this.redditoAnnuoLordo * 10) / 100
-        }else if(this.codredd == 1003){
-            this.totaleTasse=(this.redditoAnnuoLordo * 15) / 100
-        }else if(this.codredd == 1004){
-            this.totaleTasse=(this.redditoAnnuoLordo * 20) / 100
-        }else if(this.codredd == 1005){
-            this.totaleTasse=(this.redditoAnnuoLordo * 25) / 100
-        }else {
+    getUtileTasse():number{
+        if (this.codredd == 1001) {
+            return (this.redditoAnnuoLordo * 5) / 100
+        } else if (this.codredd == 1002) {
+            return (this.redditoAnnuoLordo * 10) / 100
+        } else if (this.codredd == 1003) {
+            return (this.redditoAnnuoLordo * 15) / 100
+        } else if (this.codredd == 1004) {
+            return (this.redditoAnnuoLordo * 20) / 100
+        } else if (this.codredd == 1005) {
+            return (this.redditoAnnuoLordo * 25) / 100
+        } else {
             console.log("there's been an error")
+            return 0;
         }
-    }
-    getTasseInps(){
-        return this.totaleTasse*40/100;
-    }
-    getTasseIrpef(){
-        return this.totaleTasse*60/100;
-    }
-    getRedditoAnnuoNetto(){
-        return this.redditoAnnuoLordo-this.totaleTasse;
-    }
-}
-
-class Riccone extends Persona{
-    totaleTasse:number;
-    constructor(b:number){
-        super(1005,b)
-       this.totaleTasse=0;
-    }
-    getUtileTasse(){
-        if(this.codredd == 1001){
-            this.totaleTasse=(this.redditoAnnuoLordo * 5) / 100
-        }else if(this.codredd == 1002){
-            this.totaleTasse=(this.redditoAnnuoLordo * 10) / 100
-        }else if(this.codredd == 1003){
-            this.totaleTasse=(this.redditoAnnuoLordo * 15) / 100
-        }else if(this.codredd == 1004){
-            this.totaleTasse=(this.redditoAnnuoLordo * 20) / 100
-        }else if(this.codredd == 1005){
-            this.totaleTasse=(this.redditoAnnuoLordo * 25) / 100
-        }else {
-            console.log("there's been an error")
-        }
-    }
-    getTasseInps(){
-        return this.totaleTasse*40/100;
-    }
-    getTasseIrpef(){
-        return this.totaleTasse*60/100;
-    }
-    getRedditoAnnuoNetto(){
-        return this.redditoAnnuoLordo-this.totaleTasse;
     }
 }
 
 let bagnino = new Bagnino(13000);
-console.log(bagnino.getUtileTasse(),bagnino.totaleTasse,bagnino.getTasseInps(),bagnino.getTasseIrpef(),bagnino.getRedditoAnnuoNetto());
+console.log(bagnino.getUtileTasse(), bagnino.getTasseInps(bagnino.getUtileTasse()), bagnino.getTasseIrpef(bagnino.getUtileTasse()), bagnino.getRedditoAnnuoNetto(bagnino.getUtileTasse()));
 let cassiere = new Cassiere(20000);
-console.log(cassiere.getUtileTasse(),cassiere.totaleTasse,cassiere.getTasseInps(),cassiere.getTasseIrpef(),cassiere.getRedditoAnnuoNetto());
+console.log(cassiere.getUtileTasse(), cassiere.getTasseInps(cassiere.getUtileTasse()), cassiere.getTasseIrpef(cassiere.getUtileTasse()), cassiere.getRedditoAnnuoNetto(cassiere.getUtileTasse()));
 let direttore = new Direttore(50000);
-console.log(direttore.getUtileTasse(),direttore.totaleTasse,direttore.getTasseInps(),direttore.getTasseIrpef(),direttore.getRedditoAnnuoNetto());
-let calciatore= new Calciatore(60000);
-console.log(calciatore.getUtileTasse(),calciatore.totaleTasse,calciatore.getTasseInps(),calciatore.getTasseIrpef(),calciatore.getRedditoAnnuoNetto());
+console.log(direttore.getUtileTasse(), direttore.getTasseInps(direttore.getUtileTasse()), direttore.getTasseIrpef(direttore.getUtileTasse()), direttore.getRedditoAnnuoNetto(direttore.getUtileTasse()));
+let calciatore = new Calciatore(60000);
+console.log(calciatore.getUtileTasse(), calciatore.getTasseInps(calciatore.getUtileTasse()), calciatore.getTasseIrpef(calciatore.getUtileTasse()), calciatore.getRedditoAnnuoNetto(calciatore.getUtileTasse()));
 let riccone = new Riccone(100000);
-console.log(riccone.getUtileTasse(),riccone.totaleTasse,riccone.getTasseInps(),riccone.getTasseIrpef(),riccone.getRedditoAnnuoNetto());
+console.log(riccone.getUtileTasse(), riccone.getTasseInps(riccone.getUtileTasse()), riccone.getTasseIrpef(riccone.getUtileTasse()), riccone.getRedditoAnnuoNetto(riccone.getUtileTasse()));
