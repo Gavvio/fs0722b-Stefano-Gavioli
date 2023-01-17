3a2f8cd34e5ecc2362ad55938b986317c8f4f88e
Gavvio <steve.gavvio@gmail.com>
2023-01-17 16:53:20 +0100

    week 11 day 2

--- m3/w11/d2/src/app/app.module.ts
+++ m3/w11/d2/src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DebugInterceptor } from './debug.interceptor';
-import { FavoritesComponent } from './favorites/favorites.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TdformComponent } from './tdform/tdform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FavoritesComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    TdformComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: DebugInterceptor, multi: true}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
