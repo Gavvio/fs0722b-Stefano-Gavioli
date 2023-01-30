8d3993a8419af2247bf1bca45c4e9ffbb8b3bcc3
Gavvio <steve.gavvio@gmail.com>
2023-01-30 17:07:09 +0100

    week 1 day 1 backend

---
+++ m4/w13/d1/esercizio1_epicode/src/corso_java/MainProject.java
@@ -0,0 +1,16 @@
+package corso_java;
+
+import java.util.Scanner;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("scrivi e ti copierò");
+		
+		Scanner sc= new Scanner(System.in);
+		String risposta= sc.nextLine();
+		System.out.print(risposta);
+	}
+
+}
